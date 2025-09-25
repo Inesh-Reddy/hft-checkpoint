54cea8973569105c5b0eddd1eb7d2aec72005f6f
Inesh-Reddy <ineshreddy007@gmail.com>
2025-09-25 08:36:54 +0530

    added protobuf for gRPC

--- packages/golib/redis/redis.go
+++ packages/golib/redis/redis.go
@@ -1,1 +1,38 @@
 package redis
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type rdb struct {
+	client *redis.Client
+}
+
+func newClient() *rdb {
+	reddb:=redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	return &rdb{client:reddb}
+}
+
+func (rdb *rdb ) SubscribeTORedis(ctx context.Context, channel string)*redis.PubSub{
+	sub:=rdb.client.Subscribe(ctx,channel)
+	return sub
+}
+
+func (rdb *rdb ) PublishToRedis(ctx context.Context, channel string, data []byte) *redis.IntCmd{
+	pub:=rdb.client.Publish(ctx, channel, data)
+	return pub
+}
+
+func (rdb *rdb) Set(ctx context.Context, key string, members ...interface{}) *redis.IntCmd{
+	set:= rdb.client.SAdd(ctx, key, members)
+	return set
+}
+
+func (rdb *rdb) UnSet(ctx context.Context, key string, members ...interface{}) *redis.IntCmd{
+	unset:= rdb.client.SRem(ctx, key, members)
+	return unset
+}
--- packages/golib/ws/ws.go
+++ packages/golib/ws/ws.go
@@ -1,1 +1,20 @@
 package ws
+
+import (
+	"fmt"
+
+	"github.com/gorilla/websocket"
+)
+
+type ws struct{
+	websocket *websocket.Conn
+}
+
+func ConnnectToWs(WsUrl string) *ws{
+	conn,_,err:=websocket.DefaultDialer.Dial(WsUrl, nil)
+	if(err != nil){
+		fmt.Println("Error while dialing to websocket....")
+	}
+	return &ws{websocket: conn}
+}
+
